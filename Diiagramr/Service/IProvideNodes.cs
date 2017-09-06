ca574bdd1f6885ee8f001cf0ab25c9dbc21c7f47
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2017-09-06 14:01:39 -0400

    Started working on IProvideNodes

---
+++ Diiagramr/Service/IProvideNodes.cs
@@ -0,0 +1,17 @@
+﻿using Diiagramr.Model;
+using Diiagramr.ViewModel.Diagram;
+using System.Collections.Generic;
+
+namespace Diiagramr.Service
+{
+    public interface IProvideNodes
+    {
+        void RegisterNode(AbstractNodeViewModel abstractNodeViewModel);
+
+        AbstractNodeViewModel LoadNodeViewModelFromNode(DiagramNode node);
+
+        AbstractNodeViewModel CreateNodeViewModelFromName(string name);
+
+        IEnumerable<AbstractNodeViewModel> GetRegisteredNodes();
+    }
+}
